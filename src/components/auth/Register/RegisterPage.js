aa2ce40ff47ca7584e17c4ce5bd8ede685314c68
novakvova <novakvova@gmail.com>
2021-10-17 20:31:33 +0300

    Add register begin

--- src/components/auth/Register/RegisterPage.js
+++ src/components/auth/Register/RegisterPage.js
@@ -31,6 +31,10 @@ const RegisterPage = () => {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
+        }).then(resp => {
+            console.log("Good Result", resp);
+        }, bad =>{
+            console.log(bad.response.data);
         });
 
         // console.log("Server submit data", values);
@@ -101,102 +105,3 @@ const RegisterPage = () => {
 }
 
 export default RegisterPage;
-    // state = {
-    //     email: '',
-    //     password: '',
-    //     confirmPassword: '',
-    //     fio: '',
-    //     isValidation: false,
-    //     errors: {
-    //         email: '',
-    //         password: '',
-    //         confirmPassword: '',
-    //         fio: '',
-    //     }
-    // }
-
-    // onSubmitHandler = (e) => {
-    //     e.preventDefault();
-
-    //     var errors = validationFields(this.state);
-    //     const isValid = Object.keys(errors).length === 0;
-    //     if(isValid)
-    //     {
-    //         // let json = JSON.parse(this.state);
-    //         // alert(json);
-    //         console.log("send server", this.state);
-    //     }
-    //     else{
-    //         this.setState({errors: errors, isValidation: true});
-    //     }
-
-
-
-    //     //console.log("Наш стейт", this.state);
-    // }
-    // onChangeHandler = (e) => {
-    //     const {name, value} = e.target;
-    //     //console.log(name, value);
-    //     const {isValidation} = this.state;
-    //     if(isValidation)
-    //     {
-    //         const data = { ...this.state,
-    //                         [name]: value };
-    //         const errors = validationFields(data);
-    //         this.setState({[name]: value, errors: errors});
-    //     }
-    //     else
-    //         this.setState({[name]: value});
-    // }
-    // render() {
-    //     //console.log(this.state);
-    //     const {errors} = this.state;
-    //     return (
-    //         <div className="row">
-    //             <h1 className="text-center">Реєстрація</h1>
-    //             <div className="offset-md-3 col-md-6">
-    //                 <form onSubmit={this.onSubmitHandler}>
-
-    //                     <InputTextField
-    //                         field="fio"
-    //                         label="ПІБ"
-    //                         value={this.state.fio}
-    //                         error={errors.fio}
-    //                         onChange={this.onChangeHandler}
-    //                      />
-
-    //                     <InputTextField
-    //                         field="email"
-    //                         label="Пошта"
-    //                         value={this.state.email}
-    //                         error={errors.email}
-    //                         onChange={this.onChangeHandler}
-    //                      />
-
-    //                     <InputTextField
-    //                         field="password"
-    //                         label="Пароль"
-    //                         value={this.state.password}
-    //                         error={errors.password}
-    //                         onChange={this.onChangeHandler}
-    //                         type="password"
-    //                      />
-
-    //                      <InputTextField
-    //                         field="confirmPassword"
-    //                         label="Повтор пароль"
-    //                         value={this.state.confirmPassword}
-    //                         error={errors.confirmPassword}
-    //                         onChange={this.onChangeHandler}
-    //                         type="password"
-    //                      />
-
-    //                     <button type="submit" className="btn btn-success">Реєстрація</button>
-    //                 </form>
-    //             </div>
-    //         </div>
-    //     )
-    // }
-//}
-
-
--- src/components/common/MyPhotoInput.js
+++ src/components/common/MyPhotoInput.js
@@ -9,7 +9,7 @@ const MyPhotoInput = ({
 
     const [photo, setPhoto] = useState("https://bytes.ua/wp-content/uploads/2017/08/no-image.png");
     const [error, setError] = useState("");
-    const [field, meta] = useField(props);
+    const [meta] = useField(props);
 
     const selectImage = (event) => {
         const files= event.currentTarget.files;
@@ -38,6 +38,7 @@ const MyPhotoInput = ({
                 <img
                     src={photo}
                     width="150"
+                    alt="user img"
                 />
             </label>
             <input type="file"
